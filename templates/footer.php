4b3328f8721afd544cd3fdcc0e3b91734391883e
Gad Iradufasha <50759844+Gadrawingz@users.noreply.github.com>
2024-07-31 23:35:14 +0200

    Add files via upload
    Footer updates

--- templates/footer.php
+++ templates/footer.php
@@ -1,19 +1,32 @@
-    <div class="chatbot">
+<div class="chatbot cardy">
       <header>
         <h2>KES ltd(Chatbot)</h2>
         <span class="close-btn material-symbols-outlined">close</span>
       </header>
-      <ul class="chatbox">
-        <li class="chat incoming">
-          <span class="material-symbols-outlined">smart_toy</span>
-          <p>Its KES ltd👋 <br>How can I help you?</p>
-        </li>
-      </ul>
-      <div class="chat-input">
-        <textarea placeholder="Enter a message..." spellcheck="false" required></textarea>
-        <span id="send-btn" class="material-symbols-rounded">send</span>
+      
+      <div class="chatbox" style="overflow: scroll">
+        <div id="message-section">
+          <!--<span class="material-symbols-outlined">smart_toy</span>-->
+          <div id="bot"><span id="bot-response">It's Kiboto. Type "Go" to start</span></div>
+        </div>
+
+        <div class="chat-input">
+          <textarea placeholder="Message me..." spellcheck="true" id="input" required></textarea>
+          <span id="send-btn" class="material-symbols-rounded">send</span>
+        </div>
       </div>
-    </div>
+
+      </div>
+
+      <!--
+      <div class="chat-input" id="input-section">
+      <textarea placeholder="Message me..." spellcheck="true" id="input" autofocus="autofocus"></textarea>
+      <button class="send" id="send-btn" onclick="sendMessage()">
+        <div class="circle"><i class="zmdi zmdi-mail-send"></i></div>
+      </button>
+    </div>-->
+      
+      
     
         <!-- Footer bottom -->
             <div class="footer_bottom text-center">
@@ -25,6 +38,8 @@
         </footer><!-- Footer end -->
 
         <!-- JavaScript -->
+        <script src="js/chatbot-big.js"></script>
+        <script src="js/chatbot-rules.js"></script>
         <script src="js/jquery-1.12.1.min.js"></script>
         <script src="js/bootstrap.min.js"></script>
 
@@ -35,9 +50,7 @@
         <script src="js/isotope.js"></script>
         <script src="js/isotope-active.js"></script>
         <script src="js/jquery.fancybox.js?v=2.1.5"></script>
-
         <script src="js/jquery.scrollUp.min.js"></script>
-
         <script src="js/main.js"></script>
     </body>	
 </html>	
